ff1b6f21106661e5dccaebc3d40da2c7fa06b7e5
Pham Van Minh <phamminhpro1405@gmail.com>
2025-12-01 21:31:42 +0700

    Add npm morgon

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
+
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -13,4 +16,5 @@ app.listen(port, () => {
 // npm init
 // npm install express
 // node index.js
-// npm i nodemon / npm i -g nodemon
+// npm i nodemon --save-dev / npm i -g nodemon
+// npm i morgan --save-dev
