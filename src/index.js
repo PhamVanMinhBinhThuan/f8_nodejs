ddb029571f8e7787de45612bbe12f319e049bb50
Pham Van Minh <phamminhpro1405@gmail.com>
2025-12-02 00:50:20 +0700

    Add Static file & SCSS

--- src/index.js
+++ src/index.js
@@ -6,24 +6,37 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP logger
 app.use(morgan("combined"));
 
 // Template Engine
-app.engine("hbs", handlebars.engine({
-  extname: '.hbs'
-}));
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
 app.get("/", (req, res) => {
-  res.render("home");  // file: resources/views/home.handlebars
+  res.render("home"); // file: resources/views/home.handlebars
 });
 
 app.get("/news", (req, res) => {
-  res.render("news");  // file: resources/views/home.handlebars
+  res.render("news"); // file: resources/views/home.handlebars
 });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+
+// npm init 
+// npm install express 
+// node index.js 
+// npm i nodemon --save-dev / npm i -g nodemon --> Them script "start": "nodemon index.js"
+// npm i morgan --save-dev
+// npm i node-sass --save-dev
