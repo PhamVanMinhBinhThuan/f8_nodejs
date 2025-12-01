9a28399083d9005918bbe5d533e4714cf0982885
Pham Van Minh <phamminhpro1405@gmail.com>
2025-12-01 21:17:39 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// npm init
+// npm install express
+// node index.js
+// npm i nodemon / npm i -g nodemon
