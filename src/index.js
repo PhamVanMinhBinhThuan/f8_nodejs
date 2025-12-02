bd4e15df364c9ad2335021f543dcb5c7795727bd
Pham Van Minh <phamminhpro1405@gmail.com>
2025-12-02 07:14:10 +0700

    Form default behavior

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template Engine
 app.engine(
@@ -26,17 +26,21 @@ app.get("/", (req, res) => {
 });
 
 app.get("/news", (req, res) => {
+  console.log(req.query.q);
   res.render("news"); // file: resources/views/home.handlebars
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
 });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
 
-
-// npm init 
-// npm install express 
-// node index.js 
+// npm init
+// npm install express
+// node index.js
 // npm i nodemon --save-dev / npm i -g nodemon --> Them script "start": "nodemon index.js"
 // npm i morgan --save-dev
 // npm i node-sass --save-dev
