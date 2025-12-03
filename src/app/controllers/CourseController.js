b67f0e1e677f53a2567f15557aac916531599013
Pham Van Minh <phamminhpro1405@gmail.com>
2025-12-04 04:17:10 +0700

    feat(common): Add feature delete soft

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -16,14 +16,15 @@ class CourseController {
 
     // [POST] /courses/store
     store(req, res, next) {
-        console.log(req.body);
-        const formData = req.body;
+        const formData = { ...req.body };
         formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
         const course = new Course(formData);
         course
             .save()
-            .then(() => res.redirect('/'))
-            .catch((error) => {});
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     // [GET] /courses/:id/edit
@@ -42,8 +43,22 @@ class CourseController {
             .catch(next);
     }
 
-    // [DELTE] /courses/:id
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id
     destroy(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id/force
+    forceDestroy(req, res, next) {
         Course.deleteOne({ _id: req.params.id })
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
