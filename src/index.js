c88cf9bfb36f67f2da8941d12499dceaf4357e9d
Pham Van Minh <phamminhpro1405@gmail.com>
2025-12-03 19:36:25 +0700

    feat(common): Update Course

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
 
 const app = express();
@@ -20,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // HTTP logger
 // app.use(morgan("combined"));
 
@@ -28,6 +31,9 @@ app.engine(
     'hbs',
     handlebars.engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
